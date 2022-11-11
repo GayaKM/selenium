74d6067c29bdd1d10bd9a06c3802c63e4d4d8004
GayaKM <korenmargi@gmail.cpm>
2022-11-11 17:35:49 +0200

    first organized

--- AutoProject/CartPage.py
+++ AutoProject/CartPage.py
@@ -27,19 +27,21 @@ class CartPage:
         return float(price)
 
     def products_names(self):
-        """Method"""
+        """Method that returns a list of the names of the products in the cart"""
         list_names = self.driver.find_elements(By.XPATH, '//article/div/table/tbody/tr/td[2]/label')
         for i in range(len(list_names)):
             list_names[i] = list_names[i].text
         return list_names
 
     def products_quantities(self):
+        """Method that returns a list of the quantities of the products in the cart"""
         list_q = self.driver.find_elements(By.XPATH, '//ul/li/tool-tip-cart/div/table/tbody/tr/td[2]/a/label[1]')
         for i in range(len(list_q)):
             list_q[i] = self.general.find_the_int_number(list_q[i].text)
         return list_q
 
     def products_price(self):
+        """Method that returns a list of the prices of the products in the cart"""
         prices = self.driver.find_elements(By.XPATH, "//ul/li/tool-tip-cart/div/table/tbody/tr/td[3]/p")
         for i in range(len(prices)):
             prices[i] = prices[i].text
@@ -52,16 +54,19 @@ class CartPage:
         return prices
 
     def products_colors(self):
+        """Method that returns a list of the color elements of the products in the cart"""
         return self.driver.find_element(By.XPATH, '//article/div/table/tbody/tr/td[4]/span')
 
     def click_checkout(self):
+        """Method that starts the check-out presses clicks the check-out button and open the order payment page"""
         self.driver.find_element(By.ID, "checkOutButton").click()
 
     def empty_cart_message(self):
+        """Method that finds the order complete message in the order completed page"""
         return self.driver.find_element(By.XPATH, '//div/tool-tip-cart/div/div/label[2]')
 
     def zero_items_in_cart(self):
+        """Method that finds the  message that shows 0 items in an empty cart page"""
         number = self.driver.find_element(By.XPATH, "//div/tool-tip-cart/div/div/label/span").text
         number = self.general.find_the_int_number(number)
         return number
-
